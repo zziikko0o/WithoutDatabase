3184b91c02ed6d69447f5782f785da1759e64a08
zziikko0o <zziikko0o@gmail.com>
2024-12-18 23:26:05 +0100

    Projektdateien hinzufügen.

---
+++ LoginWithoutDatabase/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using LoginWithoutDatabase.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginWithoutDatabase.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+
+            if (username == "admin" && password == "1234")
+            {
+                // Redirection vers une page après connexion (par exemple, la page d'accueil)
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Message d'erreur pour l'utilisateur
+                ViewBag.ErrorMessage = "Login failed. Invalid username or password.";
+                return View();
+            }
+        }
+    }
+}
